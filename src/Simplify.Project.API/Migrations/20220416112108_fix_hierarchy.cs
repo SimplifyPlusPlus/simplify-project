168b13c12e5712f6553caeb0e503c300360c8e31
iamquoz <46261678+iamquoz@users.noreply.github.com>
2022-04-16 16:56:52 +0400

    Исправление модели отношений квартира-клиент (#12)

---
+++ src/Simplify.Project.API/Migrations/20220416112108_fix_hierarchy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simplify.Project.API.Migrations
+{
+    public partial class fix_hierarchy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApartmentRelations_Entrances_EntranceId",
+                table: "ApartmentRelations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApartmentRelations_EntranceId",
+                table: "ApartmentRelations");
+
+            migrationBuilder.DropColumn(
+                name: "EntranceId",
+                table: "ApartmentRelations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "EntranceId",
+                table: "ApartmentRelations",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApartmentRelations_EntranceId",
+                table: "ApartmentRelations",
+                column: "EntranceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApartmentRelations_Entrances_EntranceId",
+                table: "ApartmentRelations",
+                column: "EntranceId",
+                principalTable: "Entrances",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
