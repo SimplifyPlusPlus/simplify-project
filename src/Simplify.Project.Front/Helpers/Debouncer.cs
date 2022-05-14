b4e6c070785429b2d33cad053267d78b98366ad2
iamquoz <fylhtq30rjrjkjd@gmail.com>
2022-05-14 17:15:23 +0400

    comment

--- src/Simplify.Project.Front/Helpers/Debouncer.cs
+++ src/Simplify.Project.Front/Helpers/Debouncer.cs
@@ -1,15 +1,25 @@
 namespace Simplify.Project.Front.Helpers;
 
+/// <summary>
+/// Дебаунсер
+/// </summary>
 public class Debouncer
 {
+
+	/// <summary>
+	/// Откладывает выполнение функции, если пользователь совершает какое-либо действие
+	/// </summary>
+	/// <param name="action">Функция</param>
+	/// <param name="interval">Время неактивности, после которого выполнится функция</param>
+	/// <exception cref="ArgumentNullException">Функция является null</exception>
 	public static Action<T> Debounce<T>(Action<T> action, TimeSpan interval)
 	{
-		if (action == null) throw new ArgumentNullException(nameof(action));
+		ArgumentNullException.ThrowIfNull(action, nameof(action));
 
 		var last = 0;
 		return arg =>
 		{
-			var current = System.Threading.Interlocked.Increment(ref last);
+			var current = Interlocked.Increment(ref last);
 			Task.Delay(interval).ContinueWith(task =>
 			{
 				if (current == last)
